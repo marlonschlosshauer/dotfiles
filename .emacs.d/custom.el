ef9757b0ae494c6e3f32c8aca60aa2082dcd79ca
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-03-01 14:00:02 +0100

    Add custom stuff (again)

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(json-reformat emmet-mode typescript-mode web-mode groovy-mode cider evil-cleverparens clojure-mode lsp-java company-web company-restclient company yasnippet which-key use-package undo-tree tree-sitter-langs smex rainbow-mode rainbow-delimiters pdf-tools mood-line lsp-ui key-chord hl-todo forge flycheck exec-path-from-shell evil-surround evil-org evil-numbers evil-matchit evil-collection elfeed-org editorconfig doom-themes diff-hl counsel-projectile ace-window ace-jump-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t nil)))
+ '(show-paren-match-expression ((t (:background "gray92")))))
