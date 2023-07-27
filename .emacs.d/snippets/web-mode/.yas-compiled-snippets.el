6cab2c18dd543c2350836b8a616757163ad660cb
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-07-27 12:28:11 +0200

    Remove compiled files

--- .emacs.d/snippets/web-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `web-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'web-mode
-                     '(("script" "<script src=\"$0\"></script>\n" "script" nil nil nil "/Users/marlon/.emacs.d/snippets/web-mode/script" nil nil)
-                       ("html" "<!doctype html>\n<html lang=\"de\">\n  <head>\n	<meta charset=\"utf-8\">\n	<link rel=\"stylesheet\" href=\"src/css/styles.css\">\n	$0\n  </head>\n\n  <body>\n  </body>\n</html>\n" "html" nil nil nil "/Users/marlon/.emacs.d/snippets/web-mode/html" nil nil)))
-
-
-;;; Do not edit! File generated at Fri Jun 30 21:38:35 2023
