367311688fd0c30e2dff2e2b5e37b5572eec202c
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-08-28 12:30:30 +0200

    Update custom stuff

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,11 +6,13 @@
  '(custom-safe-themes
    '("5014b68d3880d21a5539af6ef40c1e257e9045d224efb3b65bf0ae7ff2a5e17a" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" default))
  '(package-selected-packages
-   '(ef-themes gptel go-mode ivy-xref pinentry epa-program yaml-mode scss-mode mmm-mode prettier-js json-reformat emmet-mode typescript-mode web-mode groovy-mode cider evil-cleverparens clojure-mode lsp-java company-web company-restclient company yasnippet which-key use-package undo-tree tree-sitter-langs smex rainbow-mode rainbow-delimiters pdf-tools mood-line lsp-ui key-chord hl-todo forge flycheck exec-path-from-shell evil-surround evil-org evil-numbers evil-matchit evil-collection elfeed-org editorconfig doom-themes diff-hl counsel-projectile ace-window ace-jump-mode))
+   '(svgo rainbow-identifiers ef-themes gptel go-mode ivy-xref pinentry epa-program yaml-mode scss-mode mmm-mode prettier-js json-reformat emmet-mode typescript-mode web-mode groovy-mode cider evil-cleverparens clojure-mode lsp-java company-web company-restclient company yasnippet which-key use-package undo-tree tree-sitter-langs smex rainbow-mode rainbow-delimiters pdf-tools mood-line lsp-ui key-chord hl-todo forge flycheck exec-path-from-shell evil-surround evil-org evil-numbers evil-matchit evil-collection elfeed-org editorconfig doom-themes diff-hl counsel-projectile ace-window ace-jump-mode))
  '(warning-suppress-log-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0))))
+ '(show-paren-match ((t nil)))
+ '(show-paren-match-expression ((t (:background "#1B1F1B")))))
